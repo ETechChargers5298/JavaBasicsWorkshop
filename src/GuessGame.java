967e7b46b109ccd09aeb497e2798562d8c6758f7
J.A.Bianchi <JABianchi@gmail.com>
2025-12-11 15:10:33 -0500

    Starter Code Comments

---
+++ src/GuessGame.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class GuessGame {
+
+    public static void main(String[] args) {
+
+        // Create a Scanner object to be able to ask for information in the console
+        
+
+        // Create a random number from 1 to 10
+        
+
+        // "Cast"/Change the double to an int
+        
+
+        // Prompt the user to type something into the console
+        
+
+        // Create a loop to repeat guesses until they get it correct
+        while(true){
+            
+            // Record the value from the User as lowercase letters and store as a String
+            
+
+            // Convert the String letters to an int
+            
+
+            // Compare the users guess int with the random int:
+            
+            
+                // If the number is correct, tell them congratulations!
+
+            
+                // If the number is wrong, tell them ???
+                
+
+        }
+
+    } //close the main method
+    
+} //close the class
